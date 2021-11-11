a2888a2be3ed1693f051bb682a56bee45e4d8b6d
kenny-kogi <mwangiken497@gmail.com>
2021-11-11 12:26:21 +0300

    string to int (atoi solution)

---
+++ string_to_int_atoi.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+This is a temporary script file.
+"""
+
+
+
+def myAtoi(self, s: str) -> int:
+    s1 = s.split()
+    a= 0
+    try:
+        for i in range(len(s1[0])):
+            if s1[0][i].isdigit():
+                a = int(s1[0][:i+1])
+    except:
+        pass
+    if a <= -2**31:
+        a = -2**31
+    elif a>= 2**31:
+        a = 2**31-1
+    return a
