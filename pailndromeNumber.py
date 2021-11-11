735c962c56507c272f45d829e38585a21f2e2e26
kenny-kogi <mwangiken497@gmail.com>
2021-11-11 12:25:41 +0300

    PailndromNumber

---
+++ pailndromeNumber.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+This is a temporary script file.
+"""
+
+
+
+def isPalindrome(self, x):
+    """
+    :type x: int
+    :rtype: bool
+    """
+    str1 = str(x)
+    return str1 == str1[::-1]
