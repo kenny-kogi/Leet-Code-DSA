d10aae601308971a3ff75f5afe14d6e0bb479cc4
kenny-kogi <mwangiken497@gmail.com>
2021-11-15 17:05:33 +0300

    container with most water

--- reverse_bit_int.py
+++ reverse_bit_int.py
@@ -1,26 +1,22 @@
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
-
 This is a temporary script file.
 """
 
 
-def reverse_bit_integer(x):
-    """
-    :type x: int
-    :rtype: int
-    """
-    res = 0
-    if x >= 0:
-        res = int(str(x)[::-1])
-    else:
-        res = int(str(x)[:0:-1])*-1
-  
-    if 2**31-1>res>-2**31:
-        return res
-    else:
-        return 0
-    
 
-print(reverse_bit_integer(-123))    
+def myAtoi(self, s: str) -> int:
+    s1 = s.split()
+    a= 0
+    try:
+        for i in range(len(s1[0])):
+            if s1[0][i].isdigit():
+                a = int(s1[0][:i+1])
+    except:
+        pass
+    if a <= -2**31:
+        a = -2**31
+    elif a>= 2**31:
+        a = 2**31-1
+    return a
